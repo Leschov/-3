2a460b857132dfeafa9f4af103d29ccfdfac4cbe
Leschov <92725337+Leschov@users.noreply.github.com>
2021-10-25 13:01:34 +0300

    Add files via upload

---
+++ Задание #3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int summa(int a, int b) {
+    
+    return a + b;
+}
+    int main() {
+        cout << abs(summa(450,-500)) << endl;
+
+        return 0;
+    }
+  
+
+
